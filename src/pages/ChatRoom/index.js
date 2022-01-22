8998e5c1b3fc2ea8b18a9bd625c60bdea20306ea
Miguel <miguelsousalima@hotmail.com>
2022-01-22 16:17:31 -0300

    Configuracao inicial

---
+++ src/pages/ChatRoom/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Text, View} from 'react-native';
+
+function ChatRoom() {
+  return (
+    <View>
+      <Text>ChatRoom</Text>
+    </View>
+  );
+}
+
+export default ChatRoom;
---
+++ src/pages/Search/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Text, View} from 'react-native';
+
+function Search() {
+  return (
+    <View>
+      <Text>Search</Text>
+    </View>
+  );
+}
+
+export default Search;
